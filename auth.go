9ca15ff6c3cf4bdcad83391dea684a62e7a2e83b
Matt Baer <matt@write.as>
2018-09-12 19:11:37 -0400

    Support logging out

--- auth.go
+++ auth.go
@@ -42,6 +42,28 @@ func (c *Client) LogIn(username, pass string) (*AuthUser, error) {
 	return nil, fmt.Errorf("Problem authenticating: %d. %v\n", status, err)
 }
 
+// LogOut logs the current user out, making the Client's current access token
+// invalid.
+func (c *Client) LogOut() error {
+	env, err := c.delete("/auth/me", nil)
+	if err != nil {
+		return err
+	}
+
+	status := env.Code
+	if status != http.StatusNoContent {
+		if status == http.StatusNotFound {
+			return fmt.Errorf("Access token is invalid or doesn't exist")
+		}
+		return fmt.Errorf("Unable to log out: %v", env.ErrorMessage)
+	}
+
+	// Logout successful, so update the Client
+	c.token = ""
+
+	return nil
+}
+
 func (c *Client) isNotLoggedIn(code int) bool {
 	if c.token == "" {
 		return false
