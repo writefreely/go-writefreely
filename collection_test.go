5e85229ef2a916e903b6eb1ba32e3ccb0385ad42
Matt Baer <matt@write.as>
2018-09-20 12:39:02 +0200

    Fix collection_test log

--- collection_test.go
+++ collection_test.go
@@ -12,7 +12,7 @@ func TestGetCollection(t *testing.T) {
 	if err != nil {
 		t.Errorf("Unexpected fetch results: %+v, err: %v\n", res, err)
 	} else {
-		t.Logf("Post: %+v", res)
+		t.Logf("Collection: %+v", res)
 		if res.Title != "write.as" {
 			t.Errorf("Unexpected fetch results: %+v\n", res)
 		}
